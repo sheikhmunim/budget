4ce87e50d322c5bd278f88bfe80c5544531cb749
Sheikh Abdul Munim <sheikh00munim@gmail.com>
2026-05-12 18:32:53 +1000

    Add service worker: auto-reload on new deployment

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'budget-app-v1';
+
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (!e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
